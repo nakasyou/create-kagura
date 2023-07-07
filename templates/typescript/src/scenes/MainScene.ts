bb84b8ad3695572d0583b7b04d40a15aed9cee6e
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-07 17:33:49 +0900

    Add typescript template

---
+++ templates/typescript/src/scenes/MainScene.ts
@@ -0,0 +1,24 @@
+import {
+  Scene,
+  type SceneConstructorOptions,
+  Sprite,
+  Asset,
+} from "kagurajs"
+import GoboImage from "../../assets/gobo.svg"
+
+export class MainScene extends Scene {
+  gobo: Sprite
+  constructor (options: SceneConstructorOptions) {
+    super(options)
+    this.gobo = {} as Sprite
+  }
+  async init () {
+    this.gobo = await new Sprite().init({
+      asset: new Asset().fromURL(GoboImage)
+    })
+    this.addChild(this.gobo)
+  }
+  frame () {
+    this.gobo.x += Math.random() - 0.5
+  }
+}
