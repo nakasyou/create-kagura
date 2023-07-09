f08870c2e31ebe7eff2e2f6660e59eba2ca1992f
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-09 10:27:38 +0900

    Update create.mjs

--- create.mjs
+++ create.mjs
@@ -1,3 +1,18 @@
 import chalk from 'chalk'
+import prompts from 'prompts'
 
-console.log(chalk.blue("Create Kagura"))
+async function main () {
+  console.log(chalk.blue('Create Kagura'))
+
+  const questions = [
+    {
+      type: 'text',
+      name: 'projectName',
+      message: 'Project name'
+    }
+  ]
+  const result = await prompts(questions)
+  console.log(result)
+  
+}
+main()
