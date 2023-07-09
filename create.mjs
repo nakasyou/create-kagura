105f3c32c5bcba16848f13157755f3bfa55ed956
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-09 10:39:51 +0900

    なんとなく

--- create.mjs
+++ create.mjs
@@ -13,6 +13,26 @@ async function main () {
   ]
   const result = await prompts(questions)
   console.log(result)
+
+  const pkgManager = getPkgManager()
   
+  console.log('Inited KaguraJS!')
+  console.log('\nNext steps :')
+  console.log(`cd ${'dir'}`)
+  console.log(`${pkgManager} i`)
+  if (['yarn', 'pnpm'].includes(pkgManager)) {
+    console.log(`${pkgManager} dev`)
+  } else {
+    console.log(`${pkgManager} run dev`)
+  }
 }
+function getPkgManager() {
+  const npmUa = process.env.npm_config_user_agent ? process.env.npm_config_user_agent : ""  // get npm_config_user_agent
+
+  const pkgSpec = userAgent.split(' ')[0]
+  const pkgManager = pkgSpec.split('/')[0]
+
+  return pkgManager ? pkgManager : 'npm'
+}
+
 main()
